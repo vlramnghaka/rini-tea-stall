7b83ad22fbb69a8d600b5c54bb939feafb848f03
vlramnghaka <vlramnghaka@hotmail.com>
2025-05-20 09:43:10 +0530

    adding exit button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const handlePowerClick = () => {
+    if(confirm("I apps ka close dawn nia?")){
+      window.close();
+    }
+  }
   return(
     <header>
       <span className="headings" lang="en">Rini Tea Stall</span>
+      <div className="quit" onClick={handlePowerClick}><FontAwesomeIcon icon={faPowerOff}/></div>
     </header>
   )
 }
